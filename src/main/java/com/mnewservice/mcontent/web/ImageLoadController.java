86fbda394f550af761e1b79e6f1f39d4466dd3a2
Harri Tumelius <none@none>
2016-02-24 13:36:35 +0200

    SMB input tuning
    --HG--
    branch : STREAM

--- src/main/java/com/mnewservice/mcontent/web/ImageLoadController.java
+++ src/main/java/com/mnewservice/mcontent/web/ImageLoadController.java
@@ -10,11 +10,10 @@ import com.mnewservice.mcontent.manager.FileManager;
 import com.mnewservice.mcontent.repository.entity.FileEntity;
 
 import java.io.IOException;
-import java.io.InputStream;
-import javax.servlet.http.HttpServletResponse;
 import jcifs.smb.SmbFile;
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -39,39 +38,23 @@ public class ImageLoadController {
     @RequestMapping(value = {"/images/{fileUuid}"})
     public @ResponseBody
     ResponseEntity<byte[]> getImageFile(
-            @PathVariable("fileUuid") String fileUuid,
-            HttpServletResponse response
+            @PathVariable("fileUuid") String fileUuid
     ) {
         LOG.info("Showing image file by UUID=" + fileUuid);
-        //byte[] response = {};
         FileEntity fileToShow = fileManager.getFileByUuid(fileUuid);
         if (fileToShow != null) {
             LOG.info("Found: " + fileToShow.getPath() + "(" + fileToShow.getMimeType() + ") - " + fileToShow.getOriginalFilename());
             SmbFile file = fileManager.getSmbFileByPath(fileToShow.getPath());
-            /*
-            response = new byte[file.getContentLength()];
-            try {
-                InputStream is = file.getInputStream();
-                int b, indx = 0;
-                while ((b = is.read()) != -1) {
-                    response[indx++] = (byte) b;
-                }
-                is.close();
-            } catch (Exception ex) {
-                LOG.error("Error on reading SMB file", ex);
-                throw new RuntimeException("IOError on reading SMB file");
-            }
-            */
             String filename = fileToShow.getOriginalFilename();
             try {
-                response.setContentType(fileToShow.getMimeType());
-                response.setHeader(filename, filename);
-                IOUtils.copy(file.getInputStream(), response.getOutputStream());
-                response.flushBuffer();
+                final HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.parseMediaType(fileToShow.getMimeType()));
+                headers.setContentDispositionFormData(filename, filename);
+                return new ResponseEntity<>(IOUtils.toByteArray(file.getInputStream()), headers, HttpStatus.OK);
             } catch (IOException ex) {
-            LOG.info("Error writing file to output stream. Filename was " + filename, ex);
-            throw new RuntimeException("IOError writing file to output stream");
-        }
+                LOG.info("Error writing file to output stream. Filename was " + filename, ex);
+                throw new RuntimeException("IOError writing file to output stream");
+            }
         } else {
             LOG.error("Error file not found from the DB");
             throw new RuntimeException("Now such image in the system.");

b0c26af2fb51f33e39fbfc2f3c6e093955974343
Tommi Fredriksson <tommi.fredriksson@nolwenture.com>
2016-05-11 15:14:17 +0300

    Change sms shorturl generation code to use new BaseN with four characters

--- src/main/java/com/mnewservice/mcontent/util/ShortUrlUtils.java
+++ src/main/java/com/mnewservice/mcontent/util/ShortUrlUtils.java
@@ -1,10 +1,20 @@
 package com.mnewservice.mcontent.util;
 
-import java.math.BigInteger;
 import java.util.Random;
 
 public class ShortUrlUtils {
+    
+    private final static int DESIRED_SHORTID_CHARLENGTH = 4;
+    
+    private final static long MAX_RANDOM = (long) Math.pow(BaseN.CHARSET.length(), DESIRED_SHORTID_CHARLENGTH);
+    private final static Random RANDOMGEN = new Random();
+    
     public static String getRandomShortIdentifier() {
-        return new BigInteger(50, new Random()).toString(32);
+        // generate a random number that can be represented in certain number of characters using BaseN.CHARSET
+        // please take note of the maximum number of different IDs:
+        // = pow(BaseN.CHARSET.length(), desired_shortid_charlength)
+        
+        long randomNum = (long)(RANDOMGEN.nextDouble()* MAX_RANDOM); // safe, because random is always <1 and (int) cast floors
+        return BaseN.encode(randomNum);
     }
 }

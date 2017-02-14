1a584e1f04c289a00a4dc8faae5b04750e9b38e2
Marko Tuononen <marko.tuononen@nolwenture.com>
2017-02-14 10:46:24 +0200

    TWCMC-71: Added delivery time field.

--- src/main/java/com/mnewservice/mcontent/domain/DeliveryTime.java
+++ src/main/java/com/mnewservice/mcontent/domain/DeliveryTime.java
@@ -6,12 +6,22 @@ package com.mnewservice.mcontent.domain;
  */
 public enum DeliveryTime {
 
-    T0800,
-    T1000,
-    T1200,
-    T1400,
-    T1600,
-    T1800,
-    T2000,
-    T2200
+    T0800("08:00"),
+    T1000("10:00"),
+    T1200("12:00"),
+    T1400("14:00"),
+    T1600("16:00"),
+    T1800("18:00"),
+    T2000("20:00"),
+    T2200("22:00");
+
+    private final String text;
+
+    private DeliveryTime(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
